ef89854ff5f7c949e4c2e151a0156b143119da5b
Nir Morgulis <nirmorgo@gmail.com>
2018-10-03 22:36:46 +0300

    added docstring

--- src/data_handling.py
+++ src/data_handling.py
@@ -14,22 +14,23 @@ class AE_data():
         self.images_list = [folder_path+f for f in listdir(folder_path) if isfile(join(folder_path, f))]        
         self.Nimages = len(self.images_list)
 
-    def get_random_encoder_feed_dict(self, X, y, batch_size=1, img_resize=None):
+    def get_random_encoder_feed_dict(self, X, batch_size=1, img_resize=None):
+        '''
+        loads random batch of images from list and return a tensorflow feed dictionary
+        inputs:
+            X - will be used a key for the dictionary. can use a tf.Placeholder or a string.
+            batch_size - integer
+            img_resize - tuple of integers. for example - (360,280)
+        '''
         idx = random.sample(range(0, self.Nimages), batch_size)
         Xout = np.array(load_image(self.images_list[idx[0]], img_resize))
         Xout = np.reshape(Xout, ((1,) + Xout.shape))
         if batch_size == 1:
-            return {X: Xout, y: Xout}
+            return {X: Xout}
         else:
             for i in idx[1:]:
                Xtemp = np.array(load_image(self.images_list[i], img_resize))
                Xtemp = np.reshape(Xtemp, ((1,) + Xtemp.shape))
                Xout = np.append(Xout, Xtemp, axis=0)
-            out_dict = {X: Xout, y: Xout}
+            out_dict = {X: Xout}
         return out_dict
-
-data = AE_data()
-data.load_images_list_from_directory("data/COCO_train2017/")
-
-a = data.get_random_encoder_feed_dict('X','y',8,(320,280))
-pass
