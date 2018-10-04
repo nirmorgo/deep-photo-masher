8e7937841aeae87e36e5a0e43fc4c0a293a34999
Nir Morgulis <nirmorgo@gmail.com>
2018-10-05 00:02:37 +0300

    added encoder class with fully convolutional net

--- src/encoder.py
+++ src/encoder.py
@@ -1,7 +1,7 @@
 import tensorflow as tf 
 import numpy as np
 
-from src.net import autoencoder
+from src.net import build_full_conv_autoencoder as autoencoder
 
 class AE():
     def __init__(self, **kwargs):
@@ -9,13 +9,69 @@ class AE():
         if kwargs is not None:
             self.params = kwargs
         self.temp_folder = './tmp/'
+        self.best_loss = 1e25   # Will be used to monitor the best loss and save it
 
         with tf.variable_scope('Inputs'):
-            self.X = tf.placeholder(tf.float32, [None,None,None,3], name='X')
+            self.X = tf.placeholder(tf.float32, [None,480,480,3], name='X')
             self.global_step = tf.Variable(0, name='global_step', trainable=False) 
-
         with tf.variable_scope('AutoEncoder'):
-            self.net = autoencoder(self)
+            autoencoder(self)
+        with tf.variable_scope('Loss'):
+            self.loss = self.loss_func()
+            tf.summary.scalar('Total_Loss', self.loss)        
+        with tf.variable_scope('Optimizer'):
+            self.train_step = self.train_step_func()  
+        self.scalars = tf.summary.merge_all()
+        self.writer = tf.summary.FileWriter(self.temp_folder + '/tensorflow_logs/', graph=self.sess.graph)
+
+        with self.sess.as_default():
+            tf.global_variables_initializer().run()
+    
+    def loss_func(self):
+        with tf.variable_scope('L2_loss'):
+            B = tf.shape(self.X)[0]
+            H = tf.shape(self.X)[1]
+            W = tf.shape(self.X)[2]
+            C = tf.shape(self.X)[3]
+            elem_num = tf.cast(B*H*W*C, tf.float32)
+            loss = tf.nn.l2_loss(self.X - self.net_out) / elem_num
+        return loss
+    
+    def train_step_func(self):
+        learning_rate = self.params.get('learning_rate', 0.001)
+        self.LR = tf.Variable(learning_rate, name="LR")
+        return tf.train.AdamOptimizer(self.LR).minimize(self.loss, global_step=self.global_step)
+
+    def train(self, data, iters=2000, batch_size=4, learning_rate=None, train_set_resize=480):
+        print('Initialized Train!')
+        #iters_per_epoch = data.Nimages // batch_size
+        if learning_rate is not None:
+            self.sess.run(tf.assign(self.LR, learning_rate))
+        for i in range(iters):
+            feed_dict=data.get_random_encoder_feed_dict(X=self.X,  batch_size=batch_size, img_resize=train_set_resize)
+            train_scalars, _, g_step, current_loss = self.sess.run([self.scalars, self.train_step, self.global_step, self.loss],
+                                                        feed_dict=feed_dict)
+            self.writer.add_summary(train_scalars, g_step)
+            if g_step % 100 == 0 or i == 0:
+                print('iteration',g_step)
+                if current_loss < self.best_loss:
+                    self.save_model_to_checkpoint()
+                    self.best_loss = current_loss
+        if current_loss < self.best_loss:
+                    self.save_model_to_checkpoint()
+                    self.best_loss = current_loss
+
+    def save_model_to_checkpoint(self, path=None):
+        saver = tf.train.Saver()
+        if path is None:
+            path = self.temp_folder+'/model_files/model'
+        saver.save(self.sess, path)
+    
+    def restore_model_from_last_checkpoint(self, path=None):
+        if path is None:
+            path = self.temp_folder+'/model_files/model'
+        saver = tf.train.Saver()
+        saver.restore(self.sess, path)
 
 
 def get_session():
---
+++ src/net.py
@@ -0,0 +1,82 @@
+'''
+This file contains all the net architecture functions
+'''
+
+import tensorflow as tf
+from tensorflow.contrib.layers import conv2d, conv2d_transpose
+
+def build_full_conv_autoencoder(self):
+    # encoder
+    with tf.variable_scope('conv1'):
+        conv1 = _conv_layer(self.X, num_filters=32, filter_size=9, strides=1)
+    with tf.variable_scope('conv2'):
+        conv2 = _conv_layer(conv1, num_filters=32, filter_size=3, strides=2)
+    with tf.variable_scope('conv3'):
+        conv3 = _conv_layer(conv2, num_filters=32, filter_size=3, strides=2)
+    with tf.variable_scope('conv4'):
+        conv4 = _conv_layer(conv3, num_filters=32, filter_size=3, strides=2)
+    with tf.variable_scope('res_block1'):
+        resid1 = _residual_block(conv4, filter_size=3, filter_num=32)
+    with tf.variable_scope('res_block2'):
+        resid2 = _residual_block(resid1, filter_size=3, filter_num=32)
+    
+    # embedded space
+    self.z = resid2
+
+    # decoder
+    with tf.variable_scope('res_block3'):
+        resid3 = _residual_block(self.z, filter_size=3, filter_num=32)
+    with tf.variable_scope('res_block4'):
+        resid4 = _residual_block(resid3, filter_size=3, filter_num=32)
+    with tf.variable_scope('upsample1'):
+        upsample1 = _upsample(resid4, num_filters=32, filter_size=3, strides=2)
+    with tf.variable_scope('upsample2'):
+        upsample2 = _upsample(upsample1, num_filters=32, filter_size=3, strides=2)
+    with tf.variable_scope('upsample3'):
+        upsample3 = _upsample(upsample2, num_filters=32, filter_size=3, strides=2)
+    with tf.variable_scope('smoothing'):
+        out = _conv_layer(upsample3, num_filters=3, filter_size=3, strides=1, relu=False)
+    with tf.variable_scope('output'):
+       self.net_out = tf.nn.tanh(out) * 255./2 + 255./2
+
+
+
+def _conv_layer(net, num_filters, filter_size, strides, relu=True, name='conv2d'):
+    net = conv2d(net, num_filters, filter_size, 
+                                         strides, padding='SAME', 
+                                         activation_fn=None,
+                                         scope=name)
+    net = _instance_norm(net)
+    if relu:
+        net = tf.nn.relu(net)
+
+    return net
+
+
+def _residual_block(net, filter_size=3, filter_num=128):
+    tmp = _conv_layer(net, filter_num, filter_size, 1, name='conv2d_1')
+    return net + _conv_layer(tmp, filter_num, filter_size, 1, relu=False, name='conv2d_2')
+
+
+def _instance_norm(net, train=True):
+    with tf.variable_scope('instance_norm'):
+        _, _, _, channels = [i.value for i in net.get_shape()]
+        var_shape = [channels]
+        mu, sigma_sq = tf.nn.moments(net, [1,2], keep_dims=True)
+        shift = tf.Variable(tf.zeros(var_shape))
+        scale = tf.Variable(tf.ones(var_shape))
+        epsilon = 1e-3
+        normalized = (net-mu)/(sigma_sq + epsilon)**(.5)
+        out = scale * normalized + shift
+    return out
+
+
+def _upsample(net, num_filters, filter_size, strides):
+    H = tf.shape(net)[1]
+    W = tf.shape(net)[2]
+    net = tf.image.resize_nearest_neighbor(net,(H*strides, W*strides),
+                                               align_corners=False, name='resize')
+    net = conv2d(net, num_filters, filter_size, 1, padding='SAME',
+                 activation_fn=None, scope='conv2d')
+    net = _instance_norm(net)
+    return tf.nn.relu(net)
