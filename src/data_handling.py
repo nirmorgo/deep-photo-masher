e77cb566fa58aec9cfc23dbe3f1431367de032ec
Nir Morgulis <nirmorgo@gmail.com>
2018-10-25 14:31:26 +0300

    vggface data handling

--- src/data_handling.py
+++ src/data_handling.py
@@ -1,13 +1,14 @@
+import os
 from os import listdir
 from os.path import isfile, join
 from src.image_utils import load_image
-import glob
+from glob import glob
 import pickle
 import numpy as np
 import random
 
 
-class AE_data():
+class COCO_data():
     '''
     this data handler is designed to work with folders of unlabaled images
     '''
@@ -36,7 +37,7 @@ class AE_data():
             self.X.append(X_temp)
         self.X = np.array(self.X)
 
-    def get_ae_feed_dict(self, X, batch_size=1, img_resize=480, preload=False):
+    def get_vae_feed_dict(self, X, batch_size=1, img_resize=480, preload=False):
         '''
         loads random batch of images from list and return a tensorflow feed dictionary
         inputs:
@@ -106,7 +107,7 @@ class CIFAR10_data():
         
         self.Nimages = len(self.X)
     
-    def get_ae_feed_dict(self, X, batch_size=None, **kwargs):
+    def get_vae_feed_dict(self, X, batch_size=None, **kwargs):
         '''
         gets a feed dictionary out of the data set.
         inputs: 
@@ -122,3 +123,45 @@ class CIFAR10_data():
         X_batch = self.X[idxs]
         self.current_train_idx += batch_size
         return {X: X_batch}
+
+
+class VGGFace_data():
+    def __init__(self):
+        self.Nimages=0
+        self.images_list = []
+        self.X = None
+        self.idx_counter = 1e15
+        self.N_preload = 20000
+    
+    def load_images_list_from_directory(self, folder_path):
+        files = []
+        pattern   = "*.jpg"        
+        for dir,_,_ in os.walk(folder_path):
+            files.extend(glob(os.path.join(dir,pattern)))
+        self.images_list = files        
+        self.Nimages = len(self.images_list)
+        
+    def get_image(self,idx,img_resize=128):
+        return load_image(self.images_list[idx], img_resize)
+    
+    def get_vae_feed_dict(self, X, batch_size=1, img_resize=128, **kwargs):
+        '''
+        loads random batch of images from list and return a tensorflow feed dictionary
+        inputs:
+            X - will be used a key for the dictionary. can use a tf.Placeholder or a string.
+            batch_size - integer
+            img_resize - integer, number of pixels for the images in the feed dict (square images)
+        '''
+        idx = random.sample(range(0, self.Nimages), batch_size)
+        Xout = np.array(load_image(self.images_list[idx[0]], img_resize))
+        Xout = np.reshape(Xout, ((1,) + Xout.shape))
+        if batch_size == 1:
+            return {X: Xout}
+        else:
+            for i in idx[1:]:
+                Xtemp = np.array(load_image(self.images_list[i], img_resize))
+                Xtemp = np.reshape(Xtemp, ((1,) + Xtemp.shape))
+                Xout = np.append(Xout, Xtemp, axis=0)
+            out_dict = {X: Xout}
+        return out_dict
+        
